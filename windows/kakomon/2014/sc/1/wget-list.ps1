be38509d682decf5e5e9ba25b78f723bc53e6676
shinoburc <dandydot@gmail.com>
2014-08-04 09:37:59 +0900

    add 2014 kakomon.

---
+++ windows/kakomon/2014/sc/1/wget-list.ps1
@@ -0,0 +1,8 @@
+
+$webClient = New-Object System.Net.WebClient 
+
+$PdfUrls = Get-Content url.txt
+
+for ( $i = 1; $i -le $PdfUrls.Length; $i++ ){
+  $webClient.DownloadFile($PdfUrls[$i - 1], ($PdfUrls[$i - 1]).Substring(($PdfUrls[$i - 1]).LastIndexOf("/") + 1))
+}
